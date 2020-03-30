858bc80fa69bb7f7a7e95004595dbe39bed16e24
USER 1337 <n.kletsun@gmail.com>
2020-03-30 13:45:16 +0300

    Added Tabled method

--- lab3.py
+++ lab3.py
@@ -25,6 +25,25 @@ def get_Fourier(x):
             Fimagine[p] += x[k]*np.sin(-2*np.pi*p*k/N)
     return FReal, Fimagine
 
+def Tabl_method(x):  # Табличний метод
+    N = len(x)
+    w = []
+    for i in range(N):
+        if i < N//4:
+            w.append((np.cos(-2*np.pi*i/N),
+                      np.sin(-2*np.pi*i/N)))
+        elif i < N//2:
+            w.append((w[i-N//4][1],
+                      -w[i-N//4][0]))
+        else:
+            w.append((-w[i-N//2][0], -w[i-N//2][1]))
+    FR = []
+    Fi = []
+    for i in range(N):
+        FR.append(sum([w[(i*j)%N][0]*x[j] for j in range(N)]))
+        Fi.append(sum([w[(i*j)%N][1]*x[j] for j in range(N)]))
+    return FR, Fi
+
 number_of_harmonics = 10
 vidlik_number = 256
 frequency = 1500
@@ -34,8 +53,23 @@ max_number = 1
 x_t = create_harmon()
 
 (FR, Fi) = get_Fourier(x_t)
+(FR_tabl, Fi_tabl) = Tabl_method(x_t)
 
-plt.plot(range(vidlik_number), FR, label='FReal')
-plt.plot(range(vidlik_number), Fi, label='Rimagine')
-plt.legend()
+fig = plt.figure()
+
+ax_1 = fig.add_subplot(2, 2, 1)
+ax_2 = fig.add_subplot(2, 2, 2)
+ax_3 = fig.add_subplot(2, 2, 3)
+ax_4 = fig.add_subplot(2, 2, 4)
+
+ax_1.plot(range(vidlik_number), FR)
+ax_2.plot(range(vidlik_number), Fi)
+ax_3.plot(range(vidlik_number), FR_tabl)
+ax_4.plot(range(vidlik_number), Fi_tabl)
+
+ax_1.set(title='FR')
+ax_2.set(title='Fi')
+ax_3.set(title='FR_tabl')
+ax_4.set(title='Fi_tabl')
+
 plt.show()
